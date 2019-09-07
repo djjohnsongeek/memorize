fef938dcd3e302824a0f9e272c4bb06f4d41871a
djjohnsongeek <danieleejohnson@gmail.com>
2019-09-07 11:05:41 -0400

    minor settings adjustments, as well as html structure for memorize.html

--- memorize/settings.py
+++ memorize/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['the-treasured-word.herokuapp.com']
+ALLOWED_HOSTS = ['the-treasured-word.herokuapp.com', '127.0.0.1']
 ADMINS = [('daniel', 'danieleejohnson@gmail.com')]
 
 # Application definition
